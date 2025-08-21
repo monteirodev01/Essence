a9cc2601eae9d1599d64ef0dcb4814120d027a03
Paulo <paulodev2003@gmail.com>
2025-08-21 01:59:31 -0300

    filtros ajustados

--- src/Componentes/Promocoes/Promocoes.tsx
+++ src/Componentes/Promocoes/Promocoes.tsx
@@ -3,13 +3,15 @@ import css from "./Promocoes.module.css";
 import Titulo from "../Titulo/Titulo";
 import { products } from "../../data/products";
 import CustomSelect from "../CustomSelect/CustomSelect";
-import addToCar from "../../assets/icons/add-shopping-cart.svg"
+import addToCar from "../../assets/icons/add-shopping-cart.svg";
 
 const Promocoes: React.FC = () => {
   const options = [
-    { label: "Opção 1", value: "1" },
-    { label: "Opção 2", value: "2" },
-    { label: "Opção 3", value: "3" },
+    { label: "Relevância", value: "1" },
+    { label: "Menor preço", value: "2" },
+    { label: "Maior preço", value: "3" },
+    { label: "Mais vendido", value: "4" },
+    { label: "Promoção", value: "5" },
   ];
 
   return (
@@ -25,17 +27,26 @@ const Promocoes: React.FC = () => {
             <a href="">
               <img src={product.img} alt={product.name} />
               <h3 className={`font-1-m-b ${css.itemTitulo}`}>{product.name}</h3>
-          </a>
+            </a>
             <p className={`font-1-xs ${css.itemDescricao}`}>
               {product.descricao}
             </p>
             <span className={`font-1-s ${css.itemPreco}`}>
-              R${product.preco} <span className={`font-1-xs ${css.itemDesconto}`}>-{product.desconto}%</span>
+              R${product.preco}{" "}
+              <span className={`font-1-xs ${css.itemDesconto}`}>
+                -{product.desconto}%
+              </span>
             </span>
-            <button className={`font-1-xs ${css.itemBtn}`}><img className={css.btnIcon} src={addToCar} alt="" /> ADD TO CART</button>
+            <button className={`font-1-xs ${css.itemBtn}`}>
+              <img className={css.btnIcon} src={addToCar} alt="" /> ADD TO CART
+            </button>
           </div>
         ))}
       </ul>
+
+      <div className={`${css.loadMore}`}>
+        <button className={`font-1-xs ${css.plusBtn}`}>CARREGAR MAIS</button>
+      </div>
     </section>
   );
 };
